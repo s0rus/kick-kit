154a01a564f37369d09bdcadabba6ff8fc4f9a14
root <root@DESKTOP-2NRMNFD>
2023-08-03 17:58:11 +0200

    fix: 🔨image size to prevent black bars from appearing on the sides at the smallest size.

--- src/components/video-seeker/video-card.tsx
+++ src/components/video-seeker/video-card.tsx
@@ -18,7 +18,7 @@ const VideoCard = ({ videoInfo }: VideoCardProps) => {
   return (
     <Card>
       <CardContent className='flex flex-row items-center'>
-        <div className='aspect-video w-[80px] h-16 flex-shrink-0 mr-2 bg-black flex items-center justify-center'>
+        <div className='aspect-video w-[80px] max-h-16 flex-shrink-0 mr-2 bg-black flex items-center justify-center'>
           {isLoading ? (
             <Skeleton className='card-image w-full h-full' />
           ) : data?.thumbnail_url ? (
@@ -31,12 +31,16 @@ const VideoCard = ({ videoInfo }: VideoCardProps) => {
           {isLoading ? (
             <Skeleton className='w-full mb-1 rounded-lg' />
           ) : (
-            <CardTitle className='text-sm max-h-full line-clamp-2 font-semibold'>{data?.title ?? 'Unknown'}</CardTitle>
+            <CardTitle className='text-sm max-h-full line-clamp-2 font-semibold'>
+              {data?.title ?? 'Unknown'}
+            </CardTitle>
           )}
           {isLoading ? (
             <Skeleton className='w-full rounded-lg' />
           ) : (
-            <CardDescription>via {data?.provider_name ?? 'Unknown'}</CardDescription>
+            <CardDescription>
+              via {data?.provider_name ?? 'Unknown'}
+            </CardDescription>
           )}
         </div>
       </CardContent>
